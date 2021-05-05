37b4988915e80cfe5b97ab13dcb5548da8605b94
nrsimonelli <nrsimonelli@gmail.com>
2021-05-05 10:22:52 -0500

    event details component

---
+++ src/components/Info/EventDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventDetails = () => {
+  return (
+    <>
+      <div className='w-full mb-6 px-4 overflow-hidden'></div>
+    </>
+  );
+};
+
+export default EventDetails;
