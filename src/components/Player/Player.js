d1d4ad8d56e0bcf64b920541dda50561e3271456
nrsimonelli <nrsimonelli@gmail.com>
2021-06-08 14:26:44 -0500

    profile placeholder

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -5,10 +5,16 @@ const Player = () => {
   return (
     <>
       <Nav />
-      <div className='flex justify-center items-center mt-12'>
-        <h1 className='bg-clip-text text-transparent bg-gradient-to-r from-blackFade-light to-blackFade-dark dark:from-whiteFade-light dark:to-whiteFade-dark text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-6 pb-1'>
-          Player Stats
-        </h1>
+      <div className='px-4 mx-auto max-w-full md:max-w-screen-md md:p-0 lg:max-w-screen-lg xl:max-w-screen-xl'>
+        <div className='flex justify-center items-center mt-12'>
+          <h1 className='bg-clip-text text-transparent bg-gradient-to-r from-blackFade-light to-blackFade-dark dark:from-whiteFade-light dark:to-whiteFade-dark text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-6 pb-1'>
+            Player Profile
+          </h1>
+        </div>
+
+        <div className='min-h-screen flex flex-col justify-start items-center '>
+          <div>Profile pages coming soon!</div>
+        </div>
       </div>
     </>
   );
