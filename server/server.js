b14e1df86a38a9c45c6828a480c6cde8aa967123
nrsimonelli <nrsimonelli@gmail.com>
2021-06-10 00:51:27 -0500

    discord strategy, passport, auth route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 
+const sessionMiddleware = require('./modules/session-middleware');
+const passport = require('./modules/discord.strategy');
+
 // Route includes
 const eventRouter = require('./routes/event.router');
 const authRouter = require('./routes/auth.router');
@@ -9,6 +12,13 @@ const authRouter = require('./routes/auth.router');
 // Parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Passport Session Configuration //
+app.use(sessionMiddleware);
+
+// Initialize Passport Sessions
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 app.use('/api/events', eventRouter);
