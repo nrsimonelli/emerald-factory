f5d46b16200987e74b7d2268a8867cc1194faa10
nrsimonelli <nrsimonelli@gmail.com>
2021-06-30 21:07:04 -0500

    font hover state and about spacing

--- src/components/Tournament/Tournament.js
+++ src/components/Tournament/Tournament.js
@@ -10,8 +10,7 @@ const Tournament = () => {
 
       <div className='px-4 mx-auto max-w-full md:max-w-screen-md md:p-0 lg:max-w-screen-lg xl:max-w-screen-xl'>
         <div className='flex justify-center items-center mt-12'>
-          <h1 className='bg-clip-text text-transparent bg-gradient-to-r from-blackFade-light to-blackFade-dark dark:from-whiteFade-light dark:to-whiteFade-dark text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-6 pb-1'>
-            {' '}
+          <h1 className='pb-4 bg-clip-text text-transparent bg-gradient-to-r from-blackFade-light to-blackFade-dark dark:from-whiteFade-light dark:to-whiteFade-dark text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-6'>
             Scythe Events
           </h1>
         </div>
